9c974b7ecd2e730a2aa9505b86dc6b6acb9b6bbc
hyunminboo <boohyunmin@gmail.com>
2026-03-04 20:36:21 +0900

    UpLode

---
+++ src/util/collection.js
@@ -0,0 +1,30 @@
+const img = (path) => `/img/${path}`;
+
+
+export const collectionData = [
+    {
+      id: 1,
+      title: '닌텐도의 다양한 악세서리',
+      name: 'Nintendo Switch',
+      kname: '파우치&스트랩 ',
+      image: img('product_1.png')
+    },
+    {
+      id: 2,
+      title: '애플의 다양한 케이스',
+      name: 'Apple',
+      kname: '마그네틱 케이스&거치대',
+      image: img('product_2.png')
+    },
+    {
+      id: 3,
+      title: '다양한 핸드폰 악세서리',
+      name: 'Accessories',
+      kname: '맥세이프지갑 & 케이스',
+      image: img('product_3.png')
+    },
+    
+  ]
+  
+  export default collectionData
+  
