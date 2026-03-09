5bcddc86b07de0e28bde4668aab2112532a4a758
hyunminboo <boohyunmin@gmail.com>
2026-03-09 21:40:42 +0900

    elago 관련 작업 및 코드 수정

--- src/sections/Instargram.jsx
+++ src/sections/Instargram.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import instar from '../util/instargram'
+import './style/Instargram.scss'
 const Instargram = () => {
   return (
-    <div>
-      
+    <div className='inner instar-inner'>
+      <div className="t-wrap">
+        <h2 className="tit">Instagram</h2>
+        <p className="txt">@elago_official</p>
+      </div>
+      <ul className="instar-list">
+        {instar.map((i)=>(
+
+        <li key={i.id}>
+          <a href={i.link}
+          alt={i.alt}
+          style={{backgroundImage:`url(${i.image})`}}
+
+          // 배경 넣어보기
+          >{i.id}</a>
+        </li>
+        ))}
+      </ul>
     </div>
   )
 }
---
+++ src/util/instargram.js
@@ -0,0 +1,38 @@
+const img = (path) => `/img/${path}`;
+
+const instar = [
+  {
+    id: 1,
+    image: img("insta_1.png"),
+    alt: "상품 인스타 연결 이미지",
+    link: "/brand",
+  },
+  {
+    id: 2,
+    image: img("insta_2.png"),
+    alt: "상품 인스타 연결 이미지",
+    link: "/shop",
+  },
+  {
+    id: 3,
+    image: img("insta_3.png"),
+    alt: "상품 인스타 연결 이미지",
+    link: "/promotion",
+  },
+  {
+    id: 4,
+    image: img("insta_4.png"),
+    alt: "상품 인스타 연결 이미지",
+    link: "/community",
+  },
+    {
+    id: 5,
+    image: img("insta_5.png"),
+    alt: "상품 인스타 연결 이미지",
+    link: "/shop",
+  }
+];
+
+
+  export default instar
+  
